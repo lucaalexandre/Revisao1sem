9dc99de4645bbcf51866cc6eb456e62f4a394bb7
lucaalexandrepp <lucaalexandrepp@gmail.com>
2018-08-10 09:40:04 -0300

    Criaçao da tela "Detalhe"

---
+++ app/src/main/java/br/com/fiap/revisao1sem/DetalheActivity.kt
@@ -0,0 +1,12 @@
+package br.com.fiap.revisao1sem
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalheActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhe)
+    }
+}
